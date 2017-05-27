63990aaa3dd3a36a519f68ff447720836c748347
KitKat Cat <KitKat-Cat@users.noreply.github.com>
2017-05-27 09:27:39 -0600

    Create com.KitKatCat.Custom.js

---
+++ com.KitKatCat.Custom.js
@@ -0,0 +1,19 @@
+/**All Css Code Made By @KitKat-Cat Will Go Here And Any Or All Code May Break At Any Time In The Future.**/
+body.onload = function(){
+document.getElementById("myDIV").appendChild(''+
+'<!-- The Modal -->'+
+'<div id="myModal" class="modal" style="">'+
+  '<!-- Modal content -->'+
+  '<div id="modal-content">'+
+    '<div id="modal-header" class="white">'+
+      '<h3 id="modal-title"></h3>'+
+    '</div>'+
+    '<div>'+
+    '<p id="modal-body"></p>'+
+    '<div class="actions">'+
+	    '<button class="close" title="Close">OK</button>'+
+    '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>');
+};
