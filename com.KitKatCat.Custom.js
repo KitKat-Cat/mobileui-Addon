2edf7023cf0c35a398c237b84e970f34d1d66152
KitKat Cat <KitKat-Cat@users.noreply.github.com>
2017-05-27 09:33:35 -0600

    Update com.KitKatCat.Custom.js

--- com.KitKatCat.Custom.js
+++ com.KitKatCat.Custom.js
@@ -1,6 +1,6 @@
 /**All Css Code Made By @KitKat-Cat Will Go Here And Any Or All Code May Break At Any Time In The Future.**/
 document.body.onload = function(){
-document.getElement("body").appendChild(''+
+document.getElementByTagName("body").appendChild(''+
 '<!-- The Modal -->'+
 '<div id="myModal" class="modal" style="">'+
   '<!-- Modal content -->'+
@@ -16,4 +16,28 @@ document.getElement("body").appendChild(''+
     '</div>'+
   '</div>'+
 '</div>');
+	
+// Get the modal
+var MODAL = document.getElementById('myModal');
+
+// Get the <button> element that closes the modal
+var closeButton = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the close button then close the modal
+closeButton.onclick = function() {
+    MODAL.style.display = "none";
+}
+
+function UIalert(Message, title) {
+	var Modal = document.getElementById('myModal');
+	if (title === undefined) {
+		title = "This page says:";
+	}
+	var modalTitle = document.getElementById("modal-title");
+	var modalBody = document.getElementById("modal-body");
+        modalBody.innerHTML = Message; 
+        modalTitle.innerHTML = title;
+	Modal.style.display = "block";
+};
+  
 };
